6b532a2df85be459858bc3cba3bb76937ac20493
Cristhian <cristhianruanrezende@gmail.com>
2025-09-03 19:08:51 -0300

    Criação da base
    criação da base da brach main

---
+++ atividade/src/Funcoes.java
@@ -0,0 +1,19 @@
+public class Funcoes {
+    public Funcoes(){
+
+    }
+    public boolean saoAnagramas(String texto1, String texto2){
+        return false;
+    }
+
+    public String cifraDeCesar(String texto, int deslocamento){
+        return "";
+    }
+
+    public String encontrarMaiorPalavra(String frase){
+        return "";
+    }
+
+
+
+}
